02c55b10694d86ea9a0cce3b51502120db9ce939
dgraham <dustin.graham@nmt.edu>
2017-06-30 11:21:01 -0600

    stop using wget redirection

--- ffupdate.fish
+++ ffupdate.fish
@@ -1,6 +1,6 @@
 function ffupdate --description 'Update Firefox in ~/.local'
 	set uri "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-	set bz2 (wget -q --server-response --spider $uri 2>&1 | grep Location | sed -e 's/Location://' | tr -d '[[:space:]]')
+	set bz2 (curl -sI $uri | grep Location | sed -e 's/Location://' | tr -d '[[:space:]]')
 	printf $bz2
 	wget -nc --no-check-certificate -P /tmp $bz2
 	and tar jxvf /tmp/firefox-*.tar.bz2 -C /tmp
