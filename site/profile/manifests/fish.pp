b28f55ce11d509ae8d65963f3805489208c38b16
dgraham <dustin.graham@nmt.edu>
2017-08-30 22:40:10 -0600

    set up fish

---
+++ site/profile/manifests/fish.pp
@@ -0,0 +1,32 @@
+# Configure fish
+class profile::fish {
+
+  file { "/home/$::id/.config/fish/config.fish":
+    ensure => file,
+    source => 'puppet:///modules/profile/config.fish',
+  }
+
+  file { "/home/$::id/.config/fish/functions":
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  file { "/home/$::id/.config/fish/functions/ffupdate.fish":
+    ensure  => file,
+    source  => 'puppet:///modules/profile/ffupdate.fish',
+    require => File["/home/$::id/.config/fish/functions"],
+  }
+
+  file { "/home/$::id/.config/fish/functions/fontupdate.fish":
+    ensure  => file,
+    source  => 'puppet:///modules/profile/fontupdate.fish',
+    require => File["/home/$::id/.config/fish/functions"],
+  }
+
+  file { "/home/$::id/.config/fish/functions/padump.fish":
+    ensure  => file,
+    source  => 'puppet:///modules/profile/padump.fish',
+    require => File["/home/$::id/.config/fish/functions"],
+  }
+
+}
